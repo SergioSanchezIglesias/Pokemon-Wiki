2e286bad3582e98854e607b50586553bf0448b1b
SergioSanchezIglesias <Sergio.Sanchez32@alu.uclm.es>
2023-09-08 11:13:34 +0200

    Traducciones añadidas. La App está en español.

--- README.md
+++ README.md
@@ -5,5 +5,8 @@ Esta es una primera versión sobre una wiki acerca de los pokemon y sus tipos.
 Para todas las llamadas se ha utilizado la API ```https://pokeapi.co```.
 
 # Hacer Funcionar La Aplicación
-Para hacer uso del proyecto, basta con clonarlo y ejecutar el siguiente comando dentro de la carpeta ```pokemonAPP```: ```ng serve```.
+Para hacer uso del proyecto, basta con clonarlo y ejecutar el siguientes comandos dentro de la carpeta ```pokemonAPP```:
+1.  ```npm install```.
+2. ```ng serve```.
+
 Después en un buscador buscas por la url ```http:// localhost:4200``` y ya se tiene acceso a la aplicación.
--- pokemonApp/src/app/app.module.ts
+++ pokemonApp/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +10,10 @@ import { MainPageComponent } from './pages/main-page/main-page.component';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
 import { CardComponent } from './components/card/card.component';
 import { PokemonTypeCardComponent } from './components/pokemon-type-card/pokemon-type-card.component';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
@@ -20,7 +26,15 @@ import { PokemonTypeCardComponent } from './components/pokemon-type-card/pokemon
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (HttpLoaderFactory),
+        deps: [HttpClient]
+      },
+      defaultLanguage: 'es'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
